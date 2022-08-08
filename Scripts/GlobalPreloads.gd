b0770a3283c5d9e79f9ab673e428d404412ecbb8
Dwyriel <dwyriel333@hotmail.com>
2022-08-08 17:44:18 -0300

    Remove unnecessary scene & Rename player scene

--- Scripts/GlobalPreloads.gd
+++ Scripts/GlobalPreloads.gd
@@ -1,5 +1,5 @@
 extends Node
 
-const PlayerScene : PackedScene = preload("res://Scenes/Player0.tscn");
+const PlayerScene : PackedScene = preload("res://Scenes/Player.tscn");
 const SpriteFrameGreen : Resource = preload("res://Assets/SpriteFrameGreen.tres"); 
 const SpriteFrameRed : Resource = preload("res://Assets/SpriteFrameRed.tres");
